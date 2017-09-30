078457672fef8278228f435d46a3208b4a0600b7
David <David@192.168.1.8>
2017-09-30 14:33:20 +0100

    Segundo commit prueba

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -6,7 +6,8 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         
-        int num1=2, num2=3, suma;
+        int num1=2;
+        double num2=3.5, suma;
         suma = num1 + num2;
         System.out.println("Suma = "+suma);
     }
