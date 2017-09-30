360da4c0baffc9cb3f9b93a63369bda665a1a9b9
David <David@192.168.1.8>
2017-09-30 14:31:17 +0100

    Inicio proyecto

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,14 @@
+
+package holamundo;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        
+        int num1=2, num2=3, suma;
+        suma = num1 + num2;
+        System.out.println("Suma = "+suma);
+    }
+    
+}
